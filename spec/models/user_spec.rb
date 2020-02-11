e604349025bade31e6a504aebac29794f02bb63b
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-11 13:49:09 -0500

    Fix User specs and model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,38 +2,48 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    mock_user = User.new(name: "Vincent", email: "v@mail.com", password: "qwerty", password_confirmation: "qwerty")
+    before(:each) do
+      User.destroy_all
+      @user = User.new(name: "Vincent", email: "v@mail.com",
+                       password: "qwerty", password_confirmation: "qwerty")
+    end
     
     it 'should be be valid' do
-      expect(mock_user).to be_valid
-      mock_user.save
-      expect(mock_user.errors.full_messages).to eql([])
+      expect(@user).to be_valid
     end
 
     it 'should have the confirmation match the password' do
-      mock_user.update password: "somethingelse"
-      expect(mock_user.errors.full_messages).to eql(["Password confirmation doesn't match Password"])
+      @user.update password: "somethingelse"
+      expect(@user.errors.full_messages).to eql(["Password confirmation doesn't match Password"])
     end
 
     it "should be at least 5 characters" do
-      mock_user.password = "123"
-      expect(mock_user).not_to be_valid
+      @user.password = "123"
+      expect(@user).not_to be_valid
     end
 
-    it "should have a unique email and not be case-sensitive" do
-      mock_user.save
-      expect(mock_user).not_to be_valid
+    it "should have a unique email" do
+      @user.save
+      @user2 = User.new(name: "Vincent", email: "v@mail.com",
+                        password: "qwerty", password_confirmation: "qwerty")
+      expect(@user2).not_to be_valid
+    end
+
+    it "should ignore case for email" do
+      @user.email = "V@MAIL.COM"
+      expect(@user).to be_valid
     end
 
     it "should have a name" do
-      mock_user.update name: nil
-      expect(mock_user).not_to be_valid
+      @user.name = nil
+      @user.save
+      expect(@user).not_to be_valid
     end
 
     it "should have an email" do
-      mock_user.update email: nil
-      expect(mock_user).not_to be_valid
+      @user.email = nil
+      @user.save
+      expect(@user).not_to be_valid
     end
-
   end
 end
